26db2bd364382764379454d0353ff2ced0310861
Creeford Sanchez <creewrk@gmail.com>
2025-06-14 08:12:09 -0500

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import Home from "../src/Pages/Home";
+import Product from "../src/Pages/Product";
+import Cart from "../src/Pages/Cart";
+import SearchPage from "../src/Pages/SearchPage";
 
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='search' element={<SearchPage />} />
+        <Route exact path='product' element={<Product />} />
+        <Route exact path='cart' element={<Cart />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/Product.js
@@ -0,0 +1,9 @@
+function Product() {
+    return (
+        <>
+        <h1>PRoducts</h1>
+        </>
+    )
+}
+
+export default Product;
