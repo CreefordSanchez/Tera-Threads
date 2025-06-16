5ef9d8ed020823ea1189aedf05c0d2cfa74a4b1a
Gur512 <gromana2022@gmail.com>
2025-06-16 10:21:18 -0500

    updates-fetch function

--- src/Service/FakeStoreService.js
+++ src/Service/FakeStoreService.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 let api = 'https://fakestoreapi.com/products';
 
 export async function GetProductById(id) {
-    const promise = await axios.get(`${api}/${id}`);
-    
     try {
-        return promise.data;
+        const res = await axios.get(`${api}/${id}`);
+        return res.data;
     } catch (e) {
+        console.error("Error in GetProductById:", e);
         return null;
     }
 }
 
 export async function GetProductsByCategory(type) {
-    //single quote is not turning to ASCII. Do manualy..
-    const encodeType = encodeURIComponent(type).replace(/'/g, "%27"); 
-    const promise = await axios.get(`${api}/category/${encodeType}`);
-    
     try {
-        return promise.data;
+        const encodeType = encodeURIComponent(type).replace(/'/g, "%27");
+        const res = await axios.get(`${api}/category/${encodeType}`);
+        return res.data;
     } catch (e) {
+        console.error("Error in GetProductsByCategory:", e);
         return null;
-    }}
+    }
+}
