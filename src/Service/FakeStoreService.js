27a2ecb129862c5ec13725f9d824f02bba310a8a
Creeford Sanchez <creewrk@gmail.com>
2025-06-16 10:06:13 -0500

    comment getallcategory

--- src/Service/FakeStoreService.js
+++ src/Service/FakeStoreService.js
@@ -13,6 +13,8 @@ export async function GetProductById(id) {
 }
 
 export async function GetProductsByCategory(type) {
+    //EncodeUriCompoment will remove special characters like space because api link cant have spaces
+    //i prefer this because filer is too long to implement while this is directly fetching via passing directly in the link
     //single quote is not turning to ASCII. Do manualy..
     const encodeType = encodeURIComponent(type).replace(/'/g, "%27"); 
     const promise = await axios.get(`${api}/category/${encodeType}`);
